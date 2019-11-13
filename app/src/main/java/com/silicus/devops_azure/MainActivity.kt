247311b00ed3dc3fea6590f6ac1a1fcb9cf14f1b
Kalyani Mahankal <kalyani.mahankal@gmail.com>
2019-11-13 12:12:09 +0530

    App center integration

--- app/src/main/java/com/silicus/devops_azure/MainActivity.kt
+++ app/src/main/java/com/silicus/devops_azure/MainActivity.kt
@@ -7,12 +7,22 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "322bce4d-b342-429d-aa77-51ab28586e4b",
+            Analytics::class.java, Crashes::class.java
+        )
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
